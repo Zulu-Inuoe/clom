0759ff648b1c239c7d5353d2d6a5bc176fb737a3
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2019-05-21 12:59:15 -0400

    Export com-error and related accessors. Also un-export define-com-impl

--- src/package.lisp
+++ src/package.lisp
@@ -3,11 +3,14 @@
    #:alexandria
    #:cl)
   (:export
+   #:com-error
+   #:com-error-hresult
+   #:com-error-string
+   #:check-com-error
+
    #:define-com-interface
 
    #:i-unknown
    #:i-unknown-add-ref
    #:i-unknown-query-interface
-   #:i-unknown-release
-
-   #:define-com-impl))
+   #:i-unknown-release))
